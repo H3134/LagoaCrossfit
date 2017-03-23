5d321028ecfcbef3e580318af199a847c8f7a601
henriqueback <henriqueaback@gmail.com>
2017-03-23 19:09:40 -0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'contato@lagoadaconceicaocrossfit.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'henriqueaback@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulário Site:  $name";
 $email_body = "Você recebeu uma nova mensagem via preenchimento do formulário no site.\n\n"."Detalhes:\n\nNome: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
 $headers = "From: noreply@lagoadaconceicaocrossfit.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
